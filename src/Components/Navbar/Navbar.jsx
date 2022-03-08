49e90b03c51e96696af015cbae1e3a9b9940b792
Rohith <marganirohith2000@gmail.com>
2022-03-08 22:10:50 +0530

    completed Navbar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import sc from "../../assets/shopping-cart.png";
+import bell from "../../assets/bell.png";
+import menu from "../../assets/menu.png";
+import wifi from "../../assets/wifi.png";
+import statusbar from "../../assets/statusbar.png";
+import battery from "../../assets/battery.png";
+import "./Navbar.css";
+function Navbar(props) {
+  return (
+    <div className="top_cont">
+      <div className="safearea_cont">
+        <div className="sa_left">
+          <span>9:41</span>
+        </div>
+        <div className="sa_right">
+          <img src={statusbar} className="sb_img"></img>
+          <img src={wifi} className="wifi_img"></img>
+          <img src={battery}></img>
+        </div>
+      </div>
+      <div className="navbar_cont">
+        <div className="navbar_left">
+          <img src={menu}></img>
+        </div>
+        <div className="navbar_right">
+          <img src={sc} className="sc_img"></img>
+          <img src={bell} className="bell_img"></img>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ src/Components/PageOne/PageOne.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./PageOne.css";
+function PageOne(props) {
+  return (
+    <div className="Pageone_cont">
+      <div className="pageone_about">
+        <p>About Diana</p>
+        <h3>A platform that looks out for you</h3>
+        <p>
+          Diana empowers you to understand how your body works so you can look
+          and feel your best
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default PageOne;
