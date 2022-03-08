ed3ab21113f04d41dbdd71c0ab52a43c5dfffbe1
Rohith <marganirohith2000@gmail.com>
2022-03-08 23:07:27 +0530

    Page_one done

--- src/Components/PageOne/PageOne.jsx
+++ src/Components/PageOne/PageOne.jsx
@@ -1,14 +1,49 @@
 import React from "react";
 import "./PageOne.css";
+import phone from "../../assets/phone.png";
+import connect from "../../assets/connect.png";
+import eco from "../../assets/eco.png";
 function PageOne(props) {
   return (
     <div className="Pageone_cont">
       <div className="pageone_about">
-        <p>About Diana</p>
-        <h3>A platform that looks out for you</h3>
-        <p>
+        <p className="about_title">About Diana</p>
+        <h3 className="about_subtitle">A platform that looks out for you</h3>
+        <p className="about_desc">
           Diana empowers you to understand how your body works so you can look
           and feel your best
+        </p>
+
+        <hr className="horizontal_line"></hr>
+      </div>
+      <div className="pageone_philosophy">
+        <h1 className="philo_title">Our Philosophy</h1>
+        <p className="philo_subtitle">
+          Sustainable wellness is a big-picture, inside out approach
+        </p>
+        <p className="philo_desc">
+          Diana is on a mission to create thoughtful, science-backed solutions
+          for women who want to make informed decisions about their health and
+          wellness.<br></br>
+          <br></br> That’s why we’ve created an integrated ecosystem of:
+        </p>
+        <img src={eco}></img>
+        <p>
+          Consciously formulated products that deliver feel-good results fast
+        </p>
+        <img src={connect}></img>
+        <p>
+          In-app mood and period tracking options that help you cultivate a
+          deeper connection with your mind and body
+        </p>
+        <img src={phone}></img>
+        <p>
+          In-app consultation portals that connect you with compassionate
+          wellness experts
+        </p>
+        <p>
+          Consider us your nerdy ally who spends their time perfecting products
+          that work because they want you to feel your best everyday.
         </p>
       </div>
     </div>
